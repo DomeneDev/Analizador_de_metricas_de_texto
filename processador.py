bd02c0ca03c66f5c2b43205965c550d19eed4038
DomeneDev <domeneprodev@gmail.com>
2026-02-03 13:07:35 +0100

    Eliminación de espacios innecesarios

--- processador.py
+++ processador.py
@@ -5,7 +5,8 @@ Código modular y no dependiente de ninguna interfaz de entrada/salida (input/pr
 # Bibliotea re para el reemplazo de caractres sobre un string
 import re
 
-def limpiar_texto(texto_bruto:str)->str:
+
+def limpiar_texto(texto_bruto: str) -> str:
     """
     Función para "limpiar" el texto introducido:
     - Convertir a minsculas
@@ -19,12 +20,12 @@ def limpiar_texto(texto_bruto:str)->str:
     # Convertir texto a minúsculas
     texto_minus = texto_bruto.lower()
     # Eliminar signos de puntiacion con re.sub()
-    texto_tratado = re.sub(r"[.,;]"," ",texto_minus)
+    texto_tratado = re.sub(r"[.,;]", " ", texto_minus)
     # Devolver el texto tratado, sin signos de puntuación en minúsculas
     return texto_tratado
 
 
-def contar_frecuencia(texto_limpio:str)->dict:
+def contar_frecuencia(texto_limpio: str) -> dict:
     """
     Función para establecer la frecuencia de las palabras en el texto
     Args:
@@ -34,27 +35,25 @@ def contar_frecuencia(texto_limpio:str)->dict:
         dict: Diccionario donde se almacena la frecuencia con la estructura {palabra:frecuencia}
     """
     # Genero diccionario vacio para guardar las cantidad de veces que se repiten las palabras
-    frecuencia_palabras ={}
-    
+    frecuencia_palabras = {}
+
     # Separo el texto por palabras y lo almaceno en una lista
     palabras = texto_limpio.split()
-    
     # Bucle para recorrer las palabras de la lista
     for palabra in palabras:
         # Si la palabra no se encuntra en el diccionario
         if palabra not in frecuencia_palabras:
             # Añado palabra y valor 1 como primera coincidencia
-            frecuencia_palabras[palabra] =1
+            frecuencia_palabras[palabra] = 1
         # Si la palabra SI se encuentra en el diccionario
         else:
             # Aumento el valor en 1
-            frecuencia_palabras[palabra]+=1
-    
+            frecuencia_palabras[palabra] += 1
     # Devuolver diccionario de frecuencia de aparición
     return frecuencia_palabras
 
 
-def generar_metricas(texto_limpio:str)-> dict:
+def generar_metricas(texto_limpio: str) -> dict:
     """
     Función para generar las métricas para el analisis
     - Contar el total de palabras
@@ -72,30 +71,24 @@ def generar_metricas(texto_limpio:str)-> dict:
     """
     # Convierto la cadena en una lista
     palabras = texto_limpio.split()
-    
     # Total de palabras es la suma de las elementos de la lista
     total_palabras = len(palabras)
     # Utilizo función max para encontrar el elemento con la mayor longitud de la lista
     palabra_mas_larga = max(palabras, key=len)
-    
     # Lógica para calcular la media
     # Variables para almacenar la longitud de la suma de todos los elementos de la lista
     suma_longitudes = 0
-    
     # Recorrer el diccionario para extraer la longitud de las claves y el número de claves.
     for palabra in palabras:
         longitud_de_la_clave = len(palabra)
         suma_longitudes += longitud_de_la_clave
-    
     # Calculo para el promedio de longitud de las claves
     promedio_longitud = round(suma_longitudes/total_palabras, 2)
-    
     # Construir diccionario con las métricas.
     metricas = {
-        "total_palabras":total_palabras,
-        "palabra_mas_larga":palabra_mas_larga,
-        "promedio_longitud":promedio_longitud
+        "total_palabras": total_palabras,
+        "palabra_mas_larga": palabra_mas_larga,
+        "promedio_longitud": promedio_longitud
     }
-    
     # Devolver el diccionario de las métricas.
     return metricas
